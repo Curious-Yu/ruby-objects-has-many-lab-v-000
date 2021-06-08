19a682b1dc0e854c8207db85fe5d37be4df58599
Wen Yu <wyu10amy@hotmail.com>
2021-06-08 05:27:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,28 @@
 class Artist
-  attr_accessor :name, :songs
-  @@song_count = 0
 
-  def initialize(name = nil)
-    @name = name
-    @songs = []
-  end
+	attr_accessor :name
+	attr_reader :songs
+
+	@@song_count = 0
+
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
 
   def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+  	song.artist = self
+  	@songs << song
+  	@@song_count += 1
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    self.add_song(song)
+  def add_song_by_name(title)
+  	song = Song.new(title)
+    add_song(song)
   end
 
   def self.song_count
-    @@song_count
+  	@@song_count
   end
+
 end
