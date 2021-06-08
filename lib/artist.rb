fb30ee9ae8aebc864e35dd274a5d20b2d56bb958
Wen Yu <wyu10amy@hotmail.com>
2021-06-08 05:57:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,14 +16,12 @@ class Artist
   def add_song(song_name)
     @songs << song_name
     song_name.artist = self
-    @@song_count += 1
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     @songs << song
     song.artist = self
-    @@song_count += 1
   end
 
 	def self.song_count
